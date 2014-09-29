1d775c4d0a8a955100d93ea450bf2118e24be321
Peter Todd <pete@petertodd.org>
2014-09-29 01:00:01 -0400

    Make CScriptNum() take nMaxNumSize as an argument
    While the existing numeric opcodes are all limited to 4-byte bignum
    arguments, new opcodes will need different limits.

--- src/script/script.h
+++ src/script/script.h
@@ -185,7 +185,10 @@ public:
         m_value = n;
     }
 
-    explicit CScriptNum(const std::vector<unsigned char>& vch)
+    static const size_t nDefaultMaxNumSize = 4;
+
+    explicit CScriptNum(const std::vector<unsigned char>& vch,
+                        const size_t nMaxNumSize = nDefaultMaxNumSize)
     {
         if (vch.size() > nMaxNumSize)
             throw scriptnum_error("CScriptNum(const std::vector<unsigned char>&) : overflow");
@@ -288,8 +291,6 @@ public:
 
         return result;
     }
-
-    static const size_t nMaxNumSize = 4;
 
 private:
     static int64_t set_vch(const std::vector<unsigned char>& vch)
