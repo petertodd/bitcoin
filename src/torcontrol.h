d57316b1111803a21a688bc5753729d2d8e2d10c
Peter Todd <pete@petertodd.org>
2015-08-26 21:42:51 -0700

    Add missing header file

---
+++ src/torcontrol.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TORCONTROL_H
+#define TORCONTROL_H
+
+#include <scheduler.h>
+
+extern const std::string DEFAULT_TOR_CONTROL;
+const bool DEFAULT_LISTEN_ONION = true;
+
+void StartTorControl(boost::thread_group& threadGroup, CScheduler& scheduler);
+void StopTorControl();
+
+#endif // TORCONTROL_H
